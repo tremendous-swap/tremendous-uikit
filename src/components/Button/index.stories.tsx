5adae89cab1284c43488b2cfd2a2549130b8dcd1
Literally Collective <bluedragon.cstar@outlook.com>
2022-01-06 06:51:19 -0300

    Update index.stories.tsx

--- src/components/Button/index.stories.tsx
+++ src/components/Button/index.stories.tsx
@@ -44,13 +44,13 @@ export const Variants: React.FC = () => {
   return (
     <BrowserRouter>
       <Row>
-        <Button as="a" href="https://tremendousswap.com" target="_blank" rel="noreferrer">
+        <Button as="a" href="https://dev.tremendousswap.com" target="_blank" rel="noreferrer">
           As an anchor
         </Button>
         <Button as={Link} to="/router-link" variant="secondary">
           As an React Router link
         </Button>
-        <Button as="a" href="https://tremendousswap.com" disabled>
+        <Button as="a" href="https://dev.tremendousswap.com" disabled>
           As an anchor (disabled)
         </Button>
       </Row>
