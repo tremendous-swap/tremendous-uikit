c9e728e9d335d40f36632499d5a7a91731c3f23b
Literally Collective <bluedragon.cstar@outlook.com>
2022-03-07 00:50:48 -0300

    Update index.stories.tsx

--- src/components/ButtonMenu/index.stories.tsx
+++ src/components/ButtonMenu/index.stories.tsx
@@ -69,13 +69,13 @@ export const AsLinks: React.FC = () => {
     <>
       <Row>
         <ButtonMenu activeIndex={0}>
-          <ButtonMenuItem as="a" href="https://dev.tremendousswap.com">
+          <ButtonMenuItem as="a" href="https://tremendousswap.com">
             Link 1
           </ButtonMenuItem>
-          <ButtonMenuItem as="a" href="https://dev.tremendousswap.com">
+          <ButtonMenuItem as="a" href="https://tremendousswap.com">
             Link 2
           </ButtonMenuItem>
-          <ButtonMenuItem as="a" href="https://dev.tremendousswap.com">
+          <ButtonMenuItem as="a" href="https://tremendousswap.com">
             Link 3
           </ButtonMenuItem>
         </ButtonMenu>
